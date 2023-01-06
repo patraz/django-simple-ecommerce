f1e372887da277e3f1ae18fbafab765dedb7139f
Patryk <patrykprause@gmail.com>
2023-01-06 11:15:30 +0100

    Changes to be committed: 	new file:   core/__init__.py 	new file:   core/admin.py 	new file:   core/apps.py 	new file:   core/migrations/__init__.py 	new file:   core/models.py 	new file:   core/tests.py 	new file:   core/views.py 	modified:   ecom/settings.py 	modified:   ecom/urls.py 	new file:   templates/index.html

--- ecom/urls.py
+++ ecom/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home')
 ]
 
-if settings.DEBUD:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
