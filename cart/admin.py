cdb52c5ed7be91eef0ba824cac47aee21217c179
Patryk <patrykprause@gmail.com>
2023-01-12 22:26:57 +0100

    Changes to be committed: 	modified:   cart/admin.py 	modified:   cart/forms.py 	modified:   cart/views.py 	modified:   templates/cart/cart.html

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Product,OrderItem, Order, ColourVariation, SizeVariation
+from .models import Product,OrderItem, Order, ColourVariation, SizeVariation, Address
 # Register your models here.
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'zip_code',
+        'address_type',
+    ]
+
+
+admin.site.register(Address, AddressAdmin)
 
 admin.site.register(Product)
 admin.site.register(OrderItem)
